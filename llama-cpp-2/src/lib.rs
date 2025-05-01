6d775bfd64a37777ddd1ff7c86294045c53751f6
AsbjornOlling <asbjornolling@gmail.com>
2025-05-01 15:44:40 +0200

    try to init backend before calling llama_time

--- llama-cpp-2/src/lib.rs
+++ llama-cpp-2/src/lib.rs
@@ -217,6 +217,7 @@ pub enum LlamaLoraAdapterRemoveError {
 /// get the time (in microseconds) according to llama.cpp
 /// ```
 /// # use llama_cpp_2::llama_time_us;
+/// let backend = llama_backend::LlamaBackend::init().unwrap();
 /// let time = llama_time_us();
 /// assert!(time > 0);
 /// ```
